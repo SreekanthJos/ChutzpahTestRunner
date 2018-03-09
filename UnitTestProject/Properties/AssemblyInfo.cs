c700aed790773b3e1446cda3b75b1920558c3d46
Sreekanth Josyula <sreekanthjo@gmail.com>
2018-03-09 18:58:53 +0530

    first commit

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca194725-3596-48d2-8e03-e384e5e57ef2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/tests/code.spec.js
@@ -0,0 +1,8 @@
+﻿
+describe("Math lib function test", function () {
+    it("it should return 10", function () {
+        var result = mathLib.add5(5);
+        expect(result).toEqual(10);
+    });
+
+});
